ce9d675b16d845cf8db4cdb5ae32643385908270
edualb <eduardoslafiq@gmail.com>
2021-06-12 20:54:33 -0300

    add angular project

---
+++ esf-e2e/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '';
+
+  onClick() {
+    this.title = 'esf-e2e'
+  }
+}
