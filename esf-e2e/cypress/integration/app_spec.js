706298b9837c8e39e75cbe880d4c8197a0ce87ae
edualb <eduardoslafiq@gmail.com>
2021-06-12 21:17:31 -0300

    add cypress

---
+++ esf-e2e/cypress/integration/app_spec.js
@@ -0,0 +1,23 @@
+describe('My First Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200');
+    })
+
+    it('Check title', () => {
+      cy.get('h1').then(e => {
+          expect(e.text()).be.equal(' app is running!')
+      })
+    })
+
+    it('Check title after click', () => {
+        cy.get('h1').then(e => {
+            expect(e.text()).be.equal(' app is running!')
+        })
+
+        cy.get('.btn').click()
+
+        cy.get('h1').then(e => {
+            expect(e.text()).be.equal('esf-e2e app is running!')
+        })
+      })
+  })
