96b66661bbc6dd6cb22078469adeafa5efabbf29
edualb <eduardoslafiq@gmail.com>
2021-06-12 23:58:35 -0300

    using backend

--- fe-e2e/src/app/app.component.ts
+++ fe-e2e/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '';
 
+  // Just for study case, the better way is to put it in service folder
+  constructor(private http: HttpClient) {}
+
   onClick() {
-    this.title = 'esf-e2e'
+    this.getTitle().subscribe(title => {
+      this.title = title
+    })
+  }
+
+  private getTitle(): Observable<any> {
+    return this.http.get<any>('/api')
+      .pipe(
+        map(res => {
+          return res.message
+        }),
+        catchError(err => {
+          return throwError(err); 
+        })
+      )
   }
 }
--- fe-e2e/src/app/app.module.ts
+++ fe-e2e/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     NoopAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
